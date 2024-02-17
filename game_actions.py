3ee6c3b7023bb62feeaded51edfdf658894b33ca
Jeanette <khojeanette@gmail.com>
2024-02-17 23:40:00 +0000

    Fixed a variable name that's repeated

--- game_actions.py
+++ game_actions.py
@@ -6,15 +6,15 @@ def choose_move(moves_dictionary):
     # Prints a string about move choices for the player
     print(f'\nChoose your move\nR - Rock\nP - Paper\nS - Scissors')
     # The input function requests a letter from the player and stores it in variable, player_choice
-    choice = getpass('\nEnter a letter: ').strip().capitalize()
+    player_choice = getpass('\nEnter a letter: ').strip().capitalize()
     # while the letter in player_choice is not in the keys of the original_moves dictionary, execute this code block
-    while choice not in moves_dictionary.keys():
+    while player_choice not in moves_dictionary.keys():
         # Input function requests the player's choice again with a reminder and reassigns the new value to the variable
         # strip function removes whitespaces before or after the input's string
         # capitalize function converts the first letter of the input's string into a capital letter
-        choice = getpass('Please enter one letter - r, p, s: ').strip().capitalize()
+        player_choice = getpass('Please enter one letter - r, p, s: ').strip().capitalize()
 
-    return choice
+    return player_choice
 
 
 def play(moves_dictionary, player_move, opponent_move):
