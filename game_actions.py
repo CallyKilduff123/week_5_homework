d85f1ea94dcf9e2b533ce02be02bc2a32485efca
Jeanette <khojeanette@gmail.com>
2024-02-17 22:57:19 +0000

    Created a choose_move function to have a player 2 option in the game

--- game_actions.py
+++ game_actions.py
@@ -1,17 +1,36 @@
+from getpass import getpass
+
+
+def choose_move(moves_dictionary):
+    # Prints a string about move choices for the player
+    print(f'\nChoose your move\nR - Rock\nP - Paper\nS - Scissors')
+    # The input function requests a letter from the player and stores it in variable, player_choice
+    choice = getpass('\nEnter a letter: ').strip().capitalize()
+    # while the letter in player_choice is not in the keys of the original_moves dictionary, execute this code block
+    while choice not in moves_dictionary.keys():
+        # Input function requests the player's choice again with a reminder and reassigns the new value to the variable
+        # strip function removes whitespaces before or after the input's string
+        # capitalize function converts the first letter of the input's string into a capital letter
+        choice = getpass('Please enter one letter - r, p, s: ').strip().capitalize()
+
+    return choice
+
+
 def play(moves_dictionary, player_move, opponent_move):
 
     winning_pairs = [('rock', 'scissors'), ('paper', 'rock'), ('scissors', 'paper')]
 
-    print(f'\nYou: {moves_dictionary[player_move].upper()} vs. Opponent: {opponent_move[1].upper()}')
+    print(f'\nYou \u25BA {moves_dictionary[player_move].upper()} vs. {moves_dictionary[opponent_move].upper()} '
+          '\u25C4 Opponent')
 
-    if player_move == opponent_move[0]:
+    if player_move == opponent_move:
 
         print(f'\nIt\'s a tie!')
 
-    elif (moves_dictionary[player_move].lower(), opponent_move[1].lower()) in winning_pairs:
+    elif (moves_dictionary[player_move].lower(), moves_dictionary[opponent_move].lower()) in winning_pairs:
 
-        print('\nYou Win!')
+        print('\nPlayer 1, you win! \u2665 \u2665 \u2665')
 
     else:
 
-        print('\nYou lose!')
+        print('\nPlayer 1, you lose!')
--- jen_game.py
+++ jen_game.py
@@ -3,32 +3,56 @@ from random import choice
 # imports game_actions module
 import game_actions
 
+# Prints a string introduction to the game
+print(f'Let\'s play Rock, Paper, Scissors!')
+
+opponent_selection = int(input('\nWho do you want to play against?\n\n1. CPU\n2. Player 2\n\nNumber: ').strip())
+
+while opponent_selection not in [1, 2]:
+
+    opponent_selection = int(input('\nChoose number 1 or 2: ').strip())
+
 # Assigns a dictionary of moves to a variable
 original_moves = {'R': 'Rock', 'P': 'Paper', 'S': 'Scissors'}
 
-# Prints a string about move choices for the player
-print(f'Let\'s play Rock, Paper, Scissors!\n\nR - Rock\nP - Paper\nS - Scissors')
+print('\n', '~' * 15, 'player 1'.upper(), '~' * 15)
 
-# The input function requests a letter from the player and stores it in variable, player_choice
-player_choice = input('\nEnter a letter: ').strip().capitalize()
+player1 = game_actions.choose_move(original_moves)
 
-# while the letter in player_choice is not in the keys of the original_moves dictionary, execute this code block
-while player_choice not in original_moves.keys():
+# # Prints a string about move choices for the player
+# print(f'\nChoose your move\nR - Rock\nP - Paper\nS - Scissors')
+#
+# # The input function requests a letter from the player and stores it in variable, player_choice
+# player_choice = getpass('\nEnter a letter: ').strip().capitalize()
 
-    # Input function requests the player's choice again with a reminder and reassigns the new value to the variable
-    # strip function removes whitespaces before or after the input's string
-    # capitalize function converts the first letter of the input's string into a capital letter
-    player_choice = input('Please enter one letter - r, p, s: ').strip().capitalize()
 
-# items method returns the original_moves dictionary as key-value pairs
-# list function creates a list storing each key-value pair as an item and tuple
-# choice function randomly chooses a key-value tuple in the list and the choice is stored in cpu_choice variable
-cpu_choice = choice(list(original_moves.items()))
+# # while the letter in player_choice is not in the keys of the original_moves dictionary, execute this code block
+# while player_choice not in original_moves.keys():
+#
+#     # Input function requests the player's choice again with a reminder and reassigns the new value to the variable
+#     # strip function removes whitespaces before or after the input's string
+#     # capitalize function converts the first letter of the input's string into a capital letter
+#     player_choice = getpass('Please enter one letter - r, p, s: ').strip().capitalize()
 
-# The play function takes three arguments when invoked:
-# original_moves: a dictionary containing available moves
-# player_choice: a single letter input representing the player's move
-# cpu_choice: a tuple containing a key-value pair representing the CPU's move
+if opponent_selection == 1:
+    # items method returns the original_moves dictionary as key-value pairs
+    # list function creates a list storing each key-value pair as an item and tuple
+    # choice function randomly chooses a key-value tuple in the list and the choice is stored in cpu_choice variable
 
-# The play function executes a single game and returns an outcome between the player and opponent
-game_actions.play(original_moves, player_choice, cpu_choice)
+    cpu_choice = choice(list(original_moves.keys()))
+
+    # The play function takes three arguments when invoked:
+    # original_moves: a dictionary containing available moves
+    # player_choice: a single letter input representing the player's move
+    # cpu_choice: a tuple containing a key-value pair representing the CPU's move
+
+    # The play function executes a single game and returns an outcome between the player and opponent
+    game_actions.play(original_moves, player1, cpu_choice)
+
+else:
+
+    print('\n', '~' * 15, 'player 2'.upper(), '~' * 15, )
+
+    player2 = game_actions.choose_move(original_moves)
+
+    game_actions.play(original_moves, player1, player2)
