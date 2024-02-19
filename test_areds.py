37375e0892dbf890651594adf224d287eadbce5f
Cally Kilduff <callykilduff@gmail.com>
2024-02-19 10:31:26 +0000

    deleted older AREDS version and added comments

--- test_areds.py
+++ test_areds.py
@@ -1,3 +1,9 @@
+# AREDS trial looked at 5 year risk of developing advanced AMD based on fundus appearance
+# risk factors = large drusen and pigment changes
+
+
+# TODO 1 - tally points for RE:
+
 def risk_factors_RE():
     drusen_re = 0
     pigment_re = 0
@@ -15,7 +21,7 @@ def risk_factors_RE():
         print("0 points")
     return drusen_re, pigment_re
 
-# tally points for LE
+# TODO 2 - tally points for LE:
 def risk_factors_LE():
     drusen_le = 0
     pigment_le = 0
@@ -34,6 +40,7 @@ def risk_factors_LE():
     return drusen_le, pigment_le
 
 
+# TODO 3 - add up points from both eyes
 def total(right, left):
     add = (right + left)
     # final = sum(add)
@@ -41,6 +48,7 @@ def total(right, left):
     return add
 
 
+# TODO 4 - turn points into % risk score
 def risk_score(score):
     if sum(score) == 0:
         print("5 year risk of developing advanced AMD = 0.5%")
--- use_test_areds.py
+++ use_test_areds.py
@@ -6,3 +6,9 @@ print("-" * 50)
 points = test_areds.total(RE, LE)
 print("-" * 50)
 risk = test_areds.risk_score(points)
+
+# call function returning points for each eye in turn
+# pass the returned values from those functions into the function that adds them together
+# pass the returned value into the function that converts points into % risk
+
+# passing values = like a game of netball :)
